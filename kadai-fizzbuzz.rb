ef5b4a5e14afa45b408f285d67cb7d0344e4b345
naoya.matsumoto <naowork12@gmail.com>
2022-05-12 08:09:28 +0000

    a

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 != 0
-    puts "Fizz"
+    return "Fizz"
   elsif num % 5 == 0 && num % 3 != 0
-    puts "Buzz"
+    return "Buzz"
   elsif num % 15 == 0
-    puts "FizzBuzz"    
+    return "FizzBuzz"    
   else 
-    puts num.to_s
+    return num.to_s
   end
 end  
   
 num_max = 100
 (1..num_max).each do |number|
-  fizzbuzz(number)
+  puts fizzbuzz(number)
 end
